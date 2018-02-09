4dfebd41b8a3c5f8c3ad5cedb708776fc6970de9
Robert Zimmerman <robert.zimmerman@sunriseid.com>
2018-02-09 10:27:01 -0800

    First fully functioning fully dynamic version.  Needs clean up, comments and paramaters for the script main

--- ProbCalc.ps1
+++ ProbCalc.ps1
@@ -8,16 +8,9 @@
 
 <#
     .SYNOPSIS
-        Calculates the probabiliites of restults for die rolls and card flips.
+        Calculates the probabiliites of outcome for die rolls and card flips.
 
     .DESCRIPTION
-        Generates an array containing all possible outcomes of a random event, then counts how many times a set
-        of outcomes occurs.  For example if three 6-sided dice were reolled and results summed the number of times 18,
-        17, 16, etc occured would be counted.
-
-        EVENT is one complete occurance of a 'random' event.  (e.g. the result of rolling 3 dice, or a hand of 5 cards)
-        NODE is a single element of an event (e.g. a 1 die, or a 1 card)
-        VALUE is result of single node in an event.  (e.g. rolling a "six" on 1 die, or a "queen" on a single card)
 
 
 
@@ -59,11 +52,14 @@ $nodes = 3
 #the system being used
 $system = 'xWing'
 
+$Count = "Counting"
+$Node = "Nodetree"
+
 
 $global:aryFaces = @()               #an array of each face of a node
-$global:aryEvents = @()              #An array of every possible event.
-$global:aryEventsTemp = @()          #Temporar array used to create the array of every possible event.
-$global:aryResults = @()             #An summarizing the restuls of each possible event (e.g. how many events have three 6's)
+$global:aryResults = @()             #An array of every possible result.
+$global:aryResultsTemp = @()         #Temporar array used to create the array of every possible result.
+$global:arySummary = @()             #An array summarizing the total occurances of every possbile result (e.g. how many results have three 6's)
 
 $showDebug = $false
 $showDebug = $true
@@ -73,7 +69,7 @@ $showDebug = $false
 
 
 ######################################################################
-## New-EventTable
+## New-ResultTable
 ## Creates a table containing all the possible outcomes
 ## This routine is best suited for dice and similar models, 
 ## where each node value can occure on each die.  It is not suited
@@ -82,35 +78,29 @@ $showDebug = $false
 ######################################################################
 
 
-function New-EventTable {
+function New-ResultTable {
     param(
         [Parameter(Mandatory=$true)]
         [int]$Nodes                      #the number of items dice rolled, cards drawn, etc.
     )
     
-    #Seed the event array by creating an entry for each possible outcome
-    #for a single node
+    #Seed the result array by creating an entry for every possible result of a single node
 
     write-host "Adding Node: 1" -ForegroundColor Green
     foreach($value in $global:aryFaces){
-        $objEvent = New-Object -TypeName PSObject
-        Add-Member -InputObject $objEvent -MemberType 'NoteProperty' -Name 'Node1' -Value $value
-        $global:aryEvents += $objEvent
+        $objResult = New-Object -TypeName PSObject
+        $NameText = "$node + 1"
+        Add-Member -InputObject $objResult -MemberType 'NoteProperty' -Name $NameText -Value $value
+        $global:aryResults += $objResult
     }
 
 
     #Step through each node after the last
     for ($i = 2; $i -le $Nodes; $i++) {
-        Add-EventNode -nodeNum $i
+        Add-ResultNode -nodeNum $i
     }
 
 
-    #Add properties to each event to summarize the event (highest face, lowest face, etc.)
-    Add-EventMetaProperties
-
-
-    #display debug info
-    if($showDebug) {Display-EventTable}   
 
 
 }
@@ -119,50 +109,50 @@ function New-EventTable {
 
 
 ######################################################################
-## Add-EventMetaProperties
-## Adds properties to each node used to tally the result of that 
-## event (e.g. hightest, lowest, sums, etc.)
+## Add-ResultMetaProperties
+## Adds properties to each node used to tally the outcome of that 
+## result (e.g. hightest, lowest, sums, etc.)
 ######################################################################
 
-function Add-EventMetaProperties {
+function Add-ResultMetaProperties {
 
-    #Create an empty array to hold the new events.
-    $global:aryEventsTemp = @()
+    #Create an empty array to hold the new results.
+    $global:aryResultsTemp = @()
         
     if($showDebug ) {
-        write-host "Adding Event Tally Properties" -ForegroundColor Green
+        write-host "Adding Result Tally Properties" -ForegroundColor Green
     }
 
-    #step through each event
-    $eventCount = 0
-    foreach($event in $global:aryEvents){
+    #step through each result
+    $resultCount = 0
+    foreach($result in $global:aryResults){
 
-        $eventCount = $eventCount + 1
+        $resultCount = $resultCount + 1
         
-        #create a clone of the current event
+        #create a clone of the current result
         $TempEntry = New-Object -TypeName PSObject
-        $event.psobject.properties | ForEach-Object {
+        $result.psobject.properties | ForEach-Object {
             $TempEntry | Add-Member -MemberType $_.MemberType -Name $_.Name -Value $_.Value
         }
 
-        #add metadata about the event
-        Add-Member -InputObject $TempEntry -MemberType 'NoteProperty' -Name "EventID" -Value $eventCount
+        #add metadata about the result
+        Add-Member -InputObject $TempEntry -MemberType 'NoteProperty' -Name "ResultID" -Value $resultCount
         Add-Member -InputObject $TempEntry -MemberType 'NoteProperty' -Name "HighFace" -Value "Undefined"
         Add-Member -InputObject $TempEntry -MemberType 'NoteProperty' -Name "LowFace" -Value "Undefined"
 
-        #add metadata for each result in the results table
-        foreach($result in $aryResults) {
-            $resultText = $result.result + "Count"
-            Add-Member -InputObject $TempEntry -MemberType 'NoteProperty' -Name $resultText -Value "Undefined"
+        #add metadata to each outcome for each item listed in the summary table
+        foreach($line in $arySummary) {
+            $lineName = $line.face + $Count
+            Add-Member -InputObject $TempEntry -MemberType 'NoteProperty' -Name $lineName -Value 0
         }
     
-        #Add the new event to the new event table
-        $global:aryEventsTemp += $TempEntry
+        #Add the new result to the new result table
+        $global:aryResultsTemp += $TempEntry
 
         
     }
-    #replace the old event table with the new event table
-    $global:aryEvents = $global:aryEventsTemp
+    #replace the old result table with the new result table
+    $global:aryResults = $global:aryResultsTemp
 }
 
 
@@ -172,8 +162,8 @@ function Add-EventMetaProperties {
 
 
 ######################################################################
-## Add-EventNode
-## Steps through each existing event and adds an entry for each possible
+## Add-ResultNode
+## Steps through each existing result and adds an entry for each possible
 ## value of the current node.
 ## Example if the current table is coin flips and looks like:
 ## Heads
@@ -185,37 +175,37 @@ function Add-EventMetaProperties {
 ## Tails,Tails
 ######################################################################
 
-function Add-EventNode {
+function Add-ResultNode {
     param(
         [int]$nodeNum
     )
 
     write-host "Adding Node: $nodenum " -ForegroundColor Green
 
-    #Create an empty array to hold the new events.
-    $global:aryEventsTemp = @()
+    #Create an empty array to hold the new results.
+    $global:aryResultsTemp = @()
     
-    #step through each event
-    foreach($event in $global:aryEvents){
+    #step through each result
+    foreach($result in $global:aryResults){
         #step through each possible outcome of the current node
         foreach($value in $global:aryFaces){
 
-            #create a copy of the current event
+            #create a copy of the current result
             $TempEntry = New-Object -TypeName PSObject
-            $event.psobject.properties | ForEach-Object {
+            $result.psobject.properties | ForEach-Object {
                 $TempEntry | Add-Member -MemberType $_.MemberType -Name $_.Name -Value $_.Value
             }
             
-            #add outcome for this itteration of the node to the event
-            $name = "Node" + $nodeNum
+            #add outcome for this itteration of the node to the result
+            $name = $Node + $nodeNum
             Add-Member -InputObject $TempEntry -MemberType 'NoteProperty' -Name $name -Value $value
 
-            #Add the new event to the new event table
-            $global:aryEventsTemp += $TempEntry
+            #Add the new result to the new result table
+            $global:aryResultsTemp += $TempEntry
         }
     }
-    #replace the old event table with the new event table
-    $global:aryEvents = $global:aryEventsTemp
+    #replace the old result table with the new result table
+    $global:aryResults = $global:aryResultsTemp
 }
 
 
@@ -226,93 +216,125 @@ function Add-EventNode {
 
 
 ######################################################################
-## Build Result Array
+## Build Summary Array
 ######################################################################
-function New-RestultTable{
+function New-SummaryTable{
 
     #step through each value in the side array
-    foreach ($item in $aryFaces) {
+    foreach ($face in $aryFaces) {
         
-        #check to see if the value of the face is already in the result table
+        #check to see if the value of the face is already in the summary array
         $match = $false
-        foreach ($result in $global:aryResults) {
-            if($result.result -eq $item){
+        foreach ($line in $global:arySummary) {
+            if($line.face -eq $face){
                 $match = $true
             }
         }
     
-        #if there is NO match add the value of the face to the results array
+        #if there is NO match add the value of the face to the summary array
         if(!$match) {
-            $objResult = New-Object -TypeName PSObject
-            Add-Member -InputObject $objResult -MemberType 'NoteProperty' -Name 'Result' -Value $item
-            #add a property to the result object for each node in the event
+            $objLine = New-Object -TypeName PSObject
+            Add-Member -InputObject $objLine -MemberType 'NoteProperty' -Name 'Face' -Value $face
+            #add a property to the summary line object for each node in the result
             for($i = 1; $i -le $nodes; $i++){
-                $name = "Node" + $i
-                Add-Member -InputObject $objResult -MemberType 'NoteProperty' -Name $name -Value 0
+                $name = $Node + $i
+                Add-Member -InputObject $objLine -MemberType 'NoteProperty' -Name $name -Value 0
             }
 
-            #add the result object to the result array
-            $global:aryResults += $objResult
+            #add the line object to the summary array
+            $global:arySummary += $objLine
         }
     }
 
     #disply for debugging
-    if($showDebug) {Display-RestultsTable}
+    if($showDebug) {Display-SummaryTable}
 }
 
 
 
 
 ######################################################################
-## Add-EventTableMetaData
-## Steps through each event in the table and adds metadata (highest, lowest. etc.)
+## Tally-ResultTableMetaData
+## Steps through each result in the table and adds metadata (highest, lowest. etc.)
 ######################################################################
 
-function  Add-EventTableMetaData {
+function  Tally-ResultTableMetaData {
 
-    $eventCount = 0
 
     #step through each result
-    foreach ($event in $global:aryEvents) {
-        $eventCount = $eventCount +1        
+    for ($i = 0; $i -le $global:aryResults.count -1; $i++) {
 
-        #create a clone of the current event
-        $TempEntry = New-Object -TypeName PSObject
-        $event.psobject.properties | ForEach-Object {
-            $TempEntry | Add-Member -MemberType $_.MemberType -Name $_.Name -Value $_.Value
-        }
-
-        #add metadata to the cloned event
-        $TempEntry.LowFace = "Trout"
-        $TempEntry.HighFace = "high"
-        Add-EventMetaData 
-        
-        #write the updated cloned event back to the event array
-        $event.LowFace = $TempEntry.LowFace
-        $event.HighFace = $TempEntry.HighFace
+        #Tally the result of each individual result
+        Tally-ResultMetaData
     }
 
-    Display-EventTable
-    write-host "Event Count: $eventCount" -ForegroundColor Green
-
+    #Display-ResultTable
 }
-
-
-
-######################################################################
-## Add-EventMetaData
-## Steps through ONE event and adds metadata (highest, lowest. etc.)
-######################################################################
-
-function  Add-EventMetaData {
 
     
-    $TempEntry.LowFace ="BJ"
+        
 
-    Write-Host 
-    Write-Host "Event" -ForegroundColor Yellow
-    $event.psobject.properties | ForEach-Object {
-        write-host "$($_.name) $($_.value)"
+######################################################################
+## Add-ResultMetaData
+## Steps through ONE result and tallies the  metadata
+## - Counts the number of times each face occurs (e.g. if three faces resulted in Hit then HitCount = 3)
+######################################################################
+
+function  Tally-ResultMetaData {
+
+    
+    #Step through each property in the result
+    foreach($prop in  $global:aryResults[$i].psobject.Properties) {
+        #get the property name
+        $propName = $prop.name
+        #if the property has the string "node" in it, then its value is the face value for that node of the result.
+        if($propName -match $node) {
+            #Get the face result for that node
+            $propValue = $prop.value
+            #determine the property that will record the tally total for that face of the result record
+            $propTally = $propValue + $Count
+
+            #step through each property in the event object
+            foreach($prop in  $global:aryResults[$i].psobject.Properties) {
+                #if the name of the property matches the name of the face tally property increment it by 1
+                if($prop.name -eq $propTally) {
+                    $prop.value = $prop.value +1
+                }
+            }
+        }
+    }
+}
+
+
+
+######################################################################
+## Tally-Summarytable
+######################################################################
+
+function Tally-Summarytable {
+
+
+    #step through each result in the result array
+    foreach ($result in $aryResults) {
+
+
+        #step through each property in each result
+        foreach($prop in $result.psobject.Properties) {
+
+            #if the value of the property is greather than 0 check to see if is is a tally
+            if($prop.value -gt 0) {
+                #if the property name contains $Count then it is the tally of a face for that result
+                if($prop.name -match $Count) {
+                    #get the face the tally is for
+                    $FaceName = [string]$($prop.name).replace($Count,"")
+                    #get the number of times the face occured in this result
+                    $FaceCount = $prop.value
+                    #get the name of the tally in the summary table to be incremented
+                    $TallyName = $node + $FaceCount
+                    Increment-SummaryTable $FaceName $FaceCount
+                }
+            }
+        }
     }
 }
 
@@ -320,6 +342,41 @@ function  Add-EventMetaData {
 
 
 
+
+######################################################################
+## Increment-SummaryTable
+## Increments the appropriate enty in the summary table for the given
+## input.
+## Example if Blank, 2 is the input then this function will find
+## the "2" property of the "Blank" line object in the summary table
+## and increment the count by 1.
+######################################################################
+
+
+
+function Increment-SummaryTable {
+    param(
+        [string]$FaceName,           #The name of the face that needs to be incremented
+        [int]$Occurances             #How many time the face occured in the result being summarized
+    )
+
+    #the property conresponding to the number of occurances in the result
+    $PropOcc = $node + $Occurances
+
+    #Step through each line of the summary array
+    foreach ($line in $global:arySummary) {
+        #check if this is the line for the matching face
+        if($line.face -eq $FaceName) {
+            #Step through each property in the line item
+            foreach ($lineProp in $line.psobject.properties) {
+                #If the Occurance count of the property matches then increment the summary count
+                if($PropOcc -eq $($lineProp.name)){
+                    $lineProp.value = $lineProp.value +1
+                }
+            }
+        }
+    }
+}
 
 
 
@@ -328,25 +385,38 @@ function  Add-EventMetaData {
 ## Display various tables to the screen for debugging purposes
 ######################################################################
 
-function Display-EventTable {
-    Write-Host "Event Table" -ForegroundColor Green
-    foreach($event in $aryEvents) {
-        write-host $event -ForegroundColor Yellow
+function Display-ResultTable {
+    Write-Host 
+    Write-Host "Results Table" -ForegroundColor Green
+    foreach($result in $aryResults) {
+        write-host $result -ForegroundColor Yellow
     }
 }
 
 function Display-FacesTable {
+    Write-Host 
     Write-Host "Faces Table" -ForegroundColor Green
     foreach($face in $aryFaces) {
         write-host $face -ForegroundColor Yellow
     }
 }
 
-function Display-RestultsTable {
-    Write-Host "Results Table" -ForegroundColor Green
-    foreach($result in $aryResults) {
-        write-host $result -ForegroundColor Yellow
+function Display-SummaryTable {
+    Write-Host 
+    Write-Host "Summary Table" -ForegroundColor Green
+    foreach($line in $arySummary) {
+        write-host $line -ForegroundColor Yellow
     }
+}
+
+
+function Display-EventData {
+    Write-Host 
+    Write-Host "Event Data" -ForegroundColor Green
+    Write-Host " Nodes:  $nodes" 
+    Write-Host " Faces:  $($($global:aryFaces).count)"
+    Write-Host " Events: $($($global:aryResults).count)"
+    
 }
 
 
@@ -362,21 +432,29 @@ if ($system -eq 'xWing') {
     # Array representing one die (aka node) with one entry per face.
     # This script assumes values will be listed from lowest (left) to highest (right)
     $global:aryFaces = @("Blank","Blank","Focus","Focus","Hit","Hit","Hit","Crit")
-    $global:aryFaces = @("Blank","Blank","Blank","Focus","Focus","Evade","Evade","Evade")
     $global:aryFaces = @("Blank","Crit")
     $global:aryFaces = @("Blank","Blank","Hit","Crit")
+    $global:aryFaces = @("Blank","Blank","Blank","Focus","Focus","Evade","Evade","Evade")
+    $global:aryFaces = @("Blank","Blank","Focus","Focus","Hit","Hit","Hit","Crit")
     
-    #Create the table to store all the results
-    New-RestultTable
+    #Create the table to store the summarized totals for each face
+    New-SummaryTable
 
     #Create the table of all possible results
-    New-EventTable -Nodes $nodes
+    New-ResultTable -Nodes $nodes
 
-
-    Add-EventTableMetaData
+    #Add metadata properties to each result.  These poperties will hold sumarization data (e.g. highest face) about the result.
+    Add-ResultMetaProperties
     
-    #Display-EventTable
-    #Display-RestultsTable
+    #Count the occurances of each face in each result and record the totals in the metadata properties of each result object in the table.
+    Tally-ResultTableMetaData
+
+    #Count the occurance of each face quantitn in each result and summarize the totals in the Summary table.
+    Tally-Summarytable
+    
+    Display-EventData
+    #Display-ResultTable
+    Display-SummaryTable
     #Display-FacesTable
     
     
